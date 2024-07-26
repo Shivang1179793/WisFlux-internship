47dd194c27d3f0cc7c454f458136a26d9508a854
Shivang Baranwal <91139730+Shivang1179793@users.noreply.github.com>
2024-07-26 20:35:24 +0530

    Day - 04

---
+++ Day -04/q1.js
@@ -0,0 +1,19 @@
+/*
+Q1 - Write a JavaScript program to find the area of a triangle where lengths of the three of its sides are 5, 6, 7
+ */
+const triangleArea = (length, breadth, height) => {
+	console.log(`Length :- ${length}, Breadth :- ${breadth}, Height :- ${height}`)
+	if (isNaN(length) || isNaN(breadth) || isNaN(height)) {
+		return NaN
+	}
+	const s = (length + breadth + height) / 2
+	return `Area :- ${Math.sqrt((s * (s - length) * (s - breadth) * (s - height)))}`
+}
+
+
+const length = 5
+const breadth = 6
+const height = 7
+
+console.log(triangleArea(5, 6, 7))
+console.log(triangleArea('hello', 6, 7))
